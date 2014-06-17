354f673a3cbb72ad14b06dd34e7906bfa05a5a72
George Brindeiro <george@overdriver.com.br>
2014-06-17 20:39:30 -0300

    Fixed a few code glitches

--- sketchbook/lesson3_4_PotLoopBar/lesson3_4_PotLoopBar.ino
+++ sketchbook/lesson3_4_PotLoopBar/lesson3_4_PotLoopBar.ino
@@ -1,4 +1,4 @@
-int led = {11, 10, 9, 6, 5, 3};
+int led[] = {11, 10, 9, 6, 5, 3};
 int pot = A0;
 
 void setup()
@@ -15,7 +15,7 @@ void setup()
 
 void loop()
 {
-  int sensorValue = analogRead(sensorPin);
+  int sensorValue = analogRead(pot);
 
   int numPins = map(sensorValue, 0, 1023, 0, 6);
  
